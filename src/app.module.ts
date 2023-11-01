3ce70ccf2ab1eedbc7990090737b3c1cc4d4ada0
bayrem98 <bayremsayeh98@gmail.com>
2023-11-01 14:54:10 +0100

    adding env variable for connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,15 @@ import { UploadController } from './upload/upload.controller';
 import { UploadService } from './upload/upload.service';
 import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(`${process.env.MONGODB_URI}`),
+    // MongooseModule.forRoot(`${process.env.MONGODB_URI}`),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.gjbdf3j.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
+    ),
     UserModule,
     AuthModule,
     UploadModule,
